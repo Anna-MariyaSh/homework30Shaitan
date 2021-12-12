7ddbb1c851bd3bb6c152b09b5c778fda72e4a6ac
Anna-MariyaSh <88891571+Anna-MariyaSh@users.noreply.github.com>
2021-12-12 21:23:58 +0300

    Update solution.js

--- js/solution.js
+++ js/solution.js
@@ -5,6 +5,6 @@ let num = elements.length;
 console.log(`кол-во елементов ${num}`);
 for (let elem of elements) {
 	console.log(elem);
-	let text = elem.firstChild.nodeValue.trim();
+	let text = elem.textContent;
 	console.log(text);
 }
